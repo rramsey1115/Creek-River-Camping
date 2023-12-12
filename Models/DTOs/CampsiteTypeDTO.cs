cef6aadcadd795c33cc53d12191221deee6369ba
Ryan Ramsey <rramsey1115@gmail.com>
2023-12-12 10:32:09 -0600

    initial

---
+++ Models/DTOs/CampsiteTypeDTO.cs
@@ -0,0 +1,7 @@
+public class CampsiteTypeDTO
+{
+    public int Id { get; set; }
+    public string CampsiteTypeName { get; set; }
+    public int MaxReservationDays { get; set; }
+    public decimal FeePerNight { get; set; }
+}
